d4ba4a0471c1c222f5547ffbc2f2555f986f9460
Youssef-Hamada10 <youssef.hamada1035@gmail.com>
2025-04-20 19:55:50 +0200

    Update Course.cpp

--- Course-Registration-System/Course.cpp
+++ Course-Registration-System/Course.cpp
@@ -10,6 +10,14 @@ Course::Course(string id, string title, string syllabus, forward_list<Course> pr
     this->instructors = instructors;
 }
 
+Course::Course() {
+    // default constructor
+}
+
+void Course::setID(string ID) {
+    this->id = ID;
+}
+
 string Course::getID() {
     return id;
 }
@@ -30,8 +38,24 @@ void Course::setSyllabus(string syllabus) {
     this->syllabus = syllabus;
 }
 
+void Course::setNumOfPrerequisite(int num) {    //recently added
+    this->numOfPrerequisite = num;
+}
+
+int Course::getNumOfPrerequisite() {      //recently added
+    return numOfPrerequisite;
+}
+
 forward_list<Course> Course::getPrerequisite() {
     return prerequisites;
+}
+
+void Course::setNumOfInstructor(int num) {    //recently added
+    this->numOfInstructor = num;
+}
+
+int Course::getNumOfInstructor() {    //recently added
+    return numOfInstructor;
 }
 
 forward_list<Instructor> Course::getInstructors() {  //recently added
