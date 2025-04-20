00823046bcc12d0314e125bfe228844c1a6087a9
Youssef-Hamada10 <youssef.hamada1035@gmail.com>
2025-04-20 19:56:27 +0200

    Update Course.h

--- Course-Registration-System/Course.h
+++ Course-Registration-System/Course.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 struct Instructor {
+	string courseID;  //recently added
 	string id;
 	string name;
 	string department;
@@ -19,18 +20,26 @@ private:
 	string id;
 	string title;
 	string syllabus;
+	int numOfPrerequisite;   //recently added
 	forward_list<Course> prerequisites;
+	int numOfInstructor;   //recently added
 	forward_list<Instructor> instructors;
 	int creditHours;
 
 public:
 	Course(string id, string title, string syllabus, forward_list<Course> prerequisite, int creditHours, forward_list<Instructor> instructors);
+	Course();     //recently added
+	void setID(string); //recently added
 	string getID();  //recently added
 	string getTitle();
 	void setTitle(string title);
 	string getSyllabus();
 	void setSyllabus(string syllabus);
+	void setNumOfPrerequisite(int);  //recently added
+	int getNumOfPrerequisite();   //recently added
 	forward_list<Course> getPrerequisite();
+	void setNumOfInstructor(int);  //recently added
+	int getNumOfInstructor();   //recently added
 	forward_list<Instructor> getInstructors(); //recently added
 	void addPrerequisite(Course course);
 	void removePrerequisite(Course course);
